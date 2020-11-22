6c96a7ba6f486ea8680d4232052db7665c85162a
ChristianSheherlis <57498743+ChristianSheherlis@users.noreply.github.com>
2020-11-22 00:31:16 -0500

    changing up api usage

--- src/js/index.js
+++ src/js/index.js
@@ -5,36 +5,21 @@ console.log(DOMSelectors);
 
 import "regenerator-runtime/runtime";
 
-const query = `http://ddragon.leagueoflegends.com/cdn/10.23.1/data/en_US/champion.json`;
-const key = "F5RO5i94v6yDF4yzrrmxKcU4s6PjhQFGWgr5mMIkypCWKsLH5FI";
-const psquery = `https://api.pandascore.co/lol/champions?token=${key}`;
-
 const init = async function () {
+  const query = `http://ddragon.leagueoflegends.com/cdn/10.23.1/data/en_US/champion.json`;
   try {
-    const riotapi = await fetch(query);
-    const psapi = await fetch(psquery);
-    const riotdata = await riotapi.json();
-    const psdata = await psapi.json();
+    const response = await fetch(query);
+    const list = await response.json();
+    console.log(list);
 
-    Promise.all([riotdata, psdata]).then((values) => {
-      console.log(values);
-      values.forEach((champ) => {
-        DOMSelectors.displayContainer.insertAdjacentHTML(
-          "beforeend",
-          `<div class="champ-profile">
-        <div class="champ-profile-img">
-          <img
-            src="${champ.big_image_url}"
-            alt=""
-            class="poster"
-          />
-        </div>
-      </div>`
-        );
-      });
+    list.data.forEach((champ) => {
+      DOMSelectors.displayContainer.insertAdjacentHTML(
+        "beforeend",
+        `<p class="please-work">${champ.name}</p>`
+      );
     });
   } catch (error) {
-    console.log("fail");
+    console.log(error);
   }
 };
 
