e67d46437396e68f9e1970a4adea82b9e146824b
ChristianSheherlis <57498743+ChristianSheherlis@users.noreply.github.com>
2020-11-22 14:43:43 -0500

    functional for each + images

--- src/js/api.js
+++ src/js/api.js
@@ -1,29 +1,3 @@
-/* const lol = {
-armor:	24,
-armorperlevel:	3.8,
-attackdamage:	51.64,
-attackdamageperlevel:	3,
-attackrange:	550,
-attackspeedoffset:	null,
-attackspeedperlevel:	2.13,
-big_image_url:	"https://cdn.pandascore.co/images/lol/champion/big_image/a6cc80c956af28919735e9bace3dbefb.jpg",
-crit:	0,
-critperlevel:	0,
-hp:	504,
-hpperlevel:	82,
-hpregen:	5.5,
-hpregenperlevel:	0.5,
-id:	2734,
-image_url:	"https://cdn.pandascore.co/images/lol/champion/image/9bbf85ed488d4a14cee3583d6536c4d9.png",
-movespeed:	335,
-mp:	452,
-mpperlevel:	50,
-mpregen:	11.335,
-mpregenperlevel:	0.8,
-name:	"Zilean",
-spellblock:	30,
-spellblockperlevel:	0.5,
-};
+const lol = [];
 
 export { lol };
- */
--- src/js/index.js
+++ src/js/index.js
@@ -10,12 +10,23 @@ const init = async function () {
   try {
     const response = await fetch(query);
     const list = await response.json();
-    console.log(list);
+    //coverts the api objects into an array, allowing it to be passed through forEach
+    const champNames = Object.values(list.data);
+    console.log(champNames);
 
-    list.data.forEach((champ) => {
+    //using champ.id instead of champ.name gives the names with no spaces, which is needed if accessing a url such as the images
+    champNames.forEach((champ) => {
       DOMSelectors.displayContainer.insertAdjacentHTML(
         "beforeend",
-        `<p class="please-work">${champ.name}</p>`
+        `<div class="champ-card">
+        <div class="champ-card-front">
+          <img
+            src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champ.id}_0.jpg"
+            alt=""
+            class="poster"
+          />
+        </div>
+      </div>`
       );
     });
   } catch (error) {
@@ -24,3 +35,11 @@ const init = async function () {
 };
 
 init();
+
+/*<div class="champ-card">
+        <div class="champ-card-front">
+          <img
+            src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champ.id}_0.jpg"
+            alt=""
+            class="poster"
+          />*/
